70e90b96535baa9c7cce58a0b336eeb26091564a
Inder sein <indersein416@gmail.com>
2021-10-06 15:56:16 +0530

    upda

--- include/restconfig.php
+++ include/restconfig.php
@@ -4,7 +4,7 @@ if (session_status() == PHP_SESSION_NONE) {
 }
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 try {
-  $mysqli = new mysqli("localhost", "taxi", "password", "food-delivery");
+  $mysqli = new mysqli("localhost", "root", "password", "food-delivery");
   $mysqli->set_charset("utf8mb4");
 } catch(Exception $e) {
   error_log($e->getMessage());
